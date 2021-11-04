803af2105695e3f6dca2cbdab9270cfe9a43e843
sho610 <ec2-user@ip-172-31-54-218.ec2.internal>
2021-11-04 13:01:35 +0000

    [Finish]2-4

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,28 @@
+class TodolistsController < ApplicationController
+  def new
+
+    @list =List.new
+  end
+
+  def create
+    #データを新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    #データをデータベースに保存するためのsave目祖度実行
+    list.save
+    #トップ画面へリダイレクト
+    redirect_to'/top'
+  end
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list=List.find(params[:id])
+  end
+
+  private
+  #ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end
+end

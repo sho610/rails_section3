a65f868db9d5ce4e1e612e113ab4b172669f5408
sho610 <ec2-user@ip-172-31-54-218.ec2.internal>
2021-11-05 14:24:35 +0000

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '95cbb54bbd10d6f2d99946621c66b9ebca04e536c36101f8ab254acef48a48305c562546edafdfdcd4885ddfc9e662200d7d19f2335686f04a3006103a1ec382'
